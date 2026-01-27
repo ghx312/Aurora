f4ead348207de231c1f834122f1f58f4c9efe11f
ghx312 <wongpinrui2009@gmail.com>
2026-01-27 10:30:01 +0800

    Update Header.h

--- Encryption_System/Header.h
+++ Encryption_System/Header.h
@@ -1,12 +1,27 @@
-#ifndef ENCRYPTION_H
-#define ENCRYPTION_H
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <string>
+#include <vector>
+#include <utility>
+#include <openssl/bn.h>
+#include <winsock2.h>
 
+using std::vector;
 using std::string;
 
-string key_exchange();
-string encrypt_message(const string& msg, const string& key);
-string decrypt_message(const string& ciphertext, const string& key);
+extern const unsigned char PROTOCOL_VERSION;
+
+vector<unsigned char> Initialisation(SOCKET sock, bool is_host);
+vector<unsigned char> Random_Bytes(size_t length);
+vector<unsigned char> Hash_SHA256(vector<unsigned char>& unhashed_text);
+vector<unsigned char> ECDH_Private_Key_Generation();
+BIGNUM* ECDH_Public_Key_Calculator(const vector<unsigned char>& ECDH_private_key);
+bool ECDH_Verification(const vector<unsigned char> ECDH_public_key);
+BIGNUM* ECDH_Shared_Key(const BIGNUM* B_Public_ECDH, const BIGNUM* Private_ECDH);
+vector<unsigned char> HMAC_SHA256(const vector<unsigned char> Input, const vector<unsigned char> Salt);
+vector<unsigned char> HKDF_SHA256(const BIGNUM* Shared_Key);
+vector<unsigned char> AES_GCM_256_Encryption(const string& Plaintext, const vector<unsigned char>& Key);
+string AES_GCM_256_Decryption(const vector<unsigned char> IV_Ciphertext_Tag, const vector<unsigned char> Key);
 
 #endif
